ed5503c8100010d2ae263c68234b05efdfb39616
Karan Rana <ranak9722@gmail.com>
2026-02-24 21:38:40 +0530

    BlocConsumer add in counter page

--- lib/counter_page/counter_page.dart
+++ lib/counter_page/counter_page.dart
@@ -11,69 +11,69 @@ class CounterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<CounterBloc, CounterState>(
-      listener: (context, state) {
-        if (state is CounterIncrementState) {
-          var snackBar = SnackBar(content: Text('Incremented'));
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        }else if(state is CounterDecementState){
-          var snackBar = SnackBar(content: Text('Decremented'));
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        }
-      },
-      child: Scaffold(
-        appBar: AppBar(title: Text('Counter app using bloc')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BlocBuilder<CounterBloc, CounterState>(
-                builder: (context, state) {
-                  if (state is CounterInitialState) {
-                    return Text(
-                      state.count.toString(),
-                      style: TextStyle(fontSize: 24),
-                    );
-                  } else if (state is CounterIncrementState) {
-                    return Text(
-                      state.incCount.toString(),
-                      style: TextStyle(fontSize: 24),
-                    );
-                  } else if (state is CounterDecementState) {
-                    return Text(
-                      state.decCount.toString(),
-                      style: TextStyle(fontSize: 24),
-                    );
-                  } else {
-                    return Container();
-                  }
-                },
-              ),
-              const SizedBox(height: 40),
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter app using bloc')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocConsumer<CounterBloc,CounterState>(
+              builder: (context, state) {
+                if (state is CounterInitialState) {
+                  return Text(
+                    state.count.toString(),
+                    style: TextStyle(fontSize: 24),
+                  );
+                } else if (state is CounterIncrementState) {
+                  return Text(
+                    state.incCount.toString(),
+                    style: TextStyle(fontSize: 24),
+                  );
+                } else if (state is CounterDecementState) {
+                  return Text(
+                    state.decCount.toString(),
+                    style: TextStyle(fontSize: 24),
+                  );
+                } else {
+                  return Container();
+                }
+              },
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      BlocProvider.of<CounterBloc>(
-                        context,
-                      ).add(CounterIncrementEvent(incCount: counter++));
-                    },
-                    child: Text("+", style: TextStyle(fontSize: 24)),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      BlocProvider.of<CounterBloc>(
-                        context,
-                      ).add(CounterDecrementEvent(decCount: counter--));
-                    },
-                    child: Text("-", style: TextStyle(fontSize: 24)),
-                  ),
-                ],
-              ),
-            ],
-          ),
+              listener: (context, state) {
+                if (state is CounterIncrementState) {
+                  var snackBar = SnackBar(content: Text('Incremented'));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                } else if (state is CounterDecementState) {
+                  var snackBar = SnackBar(content: Text('Decremented'));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+              },
+            ),
+
+            const SizedBox(height: 40),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterBloc>(
+                      context,
+                    ).add(CounterIncrementEvent(incCount: counter++));
+                  },
+                  child: Text("+", style: TextStyle(fontSize: 24)),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterBloc>(
+                      context,
+                    ).add(CounterDecrementEvent(decCount: counter--));
+                  },
+                  child: Text("-", style: TextStyle(fontSize: 24)),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
